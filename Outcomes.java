6223799183b86f41c68fc2998e7950342c34d262
TheEagleCrawler <34045336+TheEagleCrawler@users.noreply.github.com>
2017-12-05 22:28:56 -0500

    Outcomes.java

---
+++ Outcomes.java
@@ -0,0 +1,25 @@
+public class Outcomes{
+  public static void outcome(int points){
+    if (points==0){
+      terribleOutcome();
+    } else if (points==1){
+      badOutcome();
+    } else if (points==2 || points==3){
+      goodOutcome();
+    } else {
+      fantasticDay();
+    }
+  }
+  public static void terribleOutcome(){
+  
+  }
+  public static void badOutcome(){
+  
+  }
+  public static void goodOutcome(){
+  
+  }
+  public static void fantasticDay(){
+  
+  }
+}
